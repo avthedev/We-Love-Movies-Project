1d5f5d335ef604eb3b09f4e5b437984c1dd2b22f
Anon Vanichyanukroh <anonvanichyanukroh@gmail.com>
2022-03-24 22:09:01 -0700

    added deve knex

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,13 @@ require("dotenv").config();
 
 const {
   DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
 } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: DATABASE_URL_DEVELOPMENT,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
